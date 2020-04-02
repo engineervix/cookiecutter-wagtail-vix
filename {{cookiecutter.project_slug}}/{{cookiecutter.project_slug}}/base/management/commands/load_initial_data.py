7a39cb1d8b40bae81aff10f46725f5cfc37df9a8
Victor <victormiti@umusebo.com>
2020-04-02 23:48:37 +0200

    codestyle: black

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base/management/commands/load_initial_data.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base/management/commands/load_initial_data.py
@@ -18,26 +18,26 @@ class Command(BaseCommand):
         """
         directories, file_names = local_storage.listdir(path)
         for directory in directories:
-            self._copy_files(local_storage, path + directory + '/')
+            self._copy_files(local_storage, path + directory + "/")
         for file_name in file_names:
             with local_storage.open(path + file_name) as file_:
                 default_storage.save(path + file_name, file_)
 
     def handle(self, **options):
-        fixtures_dir = os.path.join(settings.APPS_DIR, 'base', 'fixtures')
-        fixture_file = os.path.join(fixtures_dir, 'data.json')
+        fixtures_dir = os.path.join(settings.APPS_DIR, "base", "fixtures")
+        fixture_file = os.path.join(fixtures_dir, "data.json")
 
         print("Copying media files to configured storage...")
-        local_storage = FileSystemStorage(os.path.join(fixtures_dir, 'files'))
-        self._copy_files(local_storage, '')  # file storage paths are relative
+        local_storage = FileSystemStorage(os.path.join(fixtures_dir, "files"))
+        self._copy_files(local_storage, "")  # file storage paths are relative
 
         # Wagtail creates default Site and Page instances during install, but we already have
         # them in the data load. Remove the auto-generated ones.
-        if Site.objects.filter(hostname='localhost').exists():
-            Site.objects.get(hostname='localhost').delete()
-        if Page.objects.filter(title='Welcome to your new Wagtail site!').exists():
-            Page.objects.get(title='Welcome to your new Wagtail site!').delete()
+        if Site.objects.filter(hostname="localhost").exists():
+            Site.objects.get(hostname="localhost").delete()
+        if Page.objects.filter(title="Welcome to your new Wagtail site!").exists():
+            Page.objects.get(title="Welcome to your new Wagtail site!").delete()
 
-        call_command('loaddata', fixture_file, verbosity=0)
+        call_command("loaddata", fixture_file, verbosity=0)
 
         print("Awesome. Your data is loaded! Let's get to it...")
