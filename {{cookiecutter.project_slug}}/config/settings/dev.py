2c842edbd77ac1be03d1549ea97fb480480301e9
Victor Miti <victormiti@umusebo.com>
2020-08-05 22:07:24 +0200

    fix dev email setting

--- {{cookiecutter.project_slug}}/config/settings/dev.py
+++ {{cookiecutter.project_slug}}/config/settings/dev.py
@@ -45,7 +45,7 @@ LOGGING = {
 }
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-host
 EMAIL_HOST = "localhost"
